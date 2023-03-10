0a3aae0272ea3c6850eadc32d3211556522e88c7
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-10 18:05:38 +0530

    Adding @JsonIgnore to Question model property answer getter and adding @JsonProperty to its setter

--- backend/src/main/java/com/exam/model/exam/Question.java
+++ backend/src/main/java/com/exam/model/exam/Question.java
@@ -1,6 +1,7 @@
 package com.exam.model.exam;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -9,8 +10,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Question {
@@ -30,10 +29,91 @@ public class Question {
     @NotNull
     private String option4;
     @NotNull
-    @JsonIgnore
     private String answer;
     @Transient
     private String givenAnswer;
     @ManyToOne(fetch = FetchType.EAGER)
     private Quiz quiz;
+
+    public int getQuesId() {
+        return quesId;
+    }
+
+    public void setQuesId(int quesId) {
+        this.quesId = quesId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    @JsonIgnore
+    public String getAnswer() {
+        return answer;
+    }
+
+    @JsonProperty
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getGivenAnswer() {
+        return givenAnswer;
+    }
+
+    public void setGivenAnswer(String givenAnswer) {
+        this.givenAnswer = givenAnswer;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
 }
