d3d0556198653387363f847a5a3a7407a9a5904b
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-13 09:58:12 +0530

    Changes in UserController.java

--- backend/src/main/java/com/exam/controller/UserController.java
+++ backend/src/main/java/com/exam/controller/UserController.java
@@ -17,41 +17,45 @@ import java.util.Set;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/user")
-public class UserController{
+public class UserController {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
     @Autowired
     private UserService userService;
+
     @PostMapping("/post")
-    public ResponseEntity<User> createUser(@RequestBody User user) throws Exception{
+    public ResponseEntity<User> createUser(@RequestBody User user) throws Exception {
 
-        Set<UserRole> userRoles=new HashSet<>();
+        Set<UserRole> userRoles = new HashSet<>();
 
-        Role role=new Role();
+        Role role = new Role();
         role.setRoleName("NORMAL");
 
         //encoding password with BCryptPasswordEncoder
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-      UserRole userRole=new UserRole();
-      userRole.setUser(user);
-      userRole.setRole(role);
+        UserRole userRole = new UserRole();
+        userRole.setUser(user);
+        userRole.setRole(role);
 
         userRoles.add(userRole);
 
-    User createdUser = userService.createUser(user,userRoles);
-    return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
+        User createdUser = userService.createUser(user, userRoles);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
+
     @GetMapping("/{username}")
-    public ResponseEntity<User> getUserByUserName(@PathVariable("username") String username){
-        return new ResponseEntity<>(userService.getUserByUserName(username),HttpStatus.OK);
+    public ResponseEntity<User> getUserByUserName(@PathVariable("username") String username) {
+        return new ResponseEntity<>(userService.getUserByUserName(username), HttpStatus.OK);
     }
+
     @DeleteMapping("/{userId}")
-    public void deleteUserById(@PathVariable("userId") Long userId){
-       userService.deleteUserById(userId);
+    public void deleteUserById(@PathVariable("userId") Long userId) {
+        userService.deleteUserById(userId);
     }
+
     @ExceptionHandler(UserFoundException.class)
-    public ResponseEntity<?> exceptionHandler(UserFoundException ex){
+    public ResponseEntity<?> exceptionHandler(UserFoundException ex) {
         return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
     }
 }
